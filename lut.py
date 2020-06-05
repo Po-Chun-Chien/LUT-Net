b66e0d5f2c50993f2ecc4af3ec39715ff5995748
b04901112 <b04901112@ntu.edu.tw>
2020-06-06 03:31:12 +0800

    fix minor bug

--- lut.py
+++ lut.py
@@ -150,9 +150,9 @@ class LUT(Node):
             if self[i] == 1:
                 pats.append(format(i, fmt) + ' 1')
         if len(pats) == 2**self.k: # const 1
-            names, pats =  self.name, ['1']
+            names, pats = [self.name], ['1']
         elif len(pats) == 0: # const 0
-            names = self.name
+            names = [self.name]
         return names, pats
         
     # trains the LUT to mimic a neuron according to the input weights
